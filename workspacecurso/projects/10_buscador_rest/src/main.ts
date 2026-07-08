f57c75155d99d17866cda12e60d399961c747f22
amartinsierra <ajms66@gmail.com>
2026-07-08 12:57:55 +0200

    micro servicio buscador routing

--- workspacecurso/projects/10_buscador_rest/src/main.ts
+++ workspacecurso/projects/10_buscador_rest/src/main.ts
@@ -3,5 +3,6 @@ import { appConfig } from './app/app.config';
 
 import { BuscarController } from './app/controller/buscar-controller/buscar-controller';
 import { AltaController } from './app/controller/alta-controller/alta-controller';
+import { MenuController } from './app/controller/menu-controller/menu-controller';
 
-bootstrapApplication(AltaController, appConfig).catch((err) => console.error(err));
+bootstrapApplication(MenuController, appConfig).catch((err) => console.error(err));
